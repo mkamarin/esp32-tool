828e09354f708b68d628b8580cc714c263903d3e
Mike Marin <ww6mm@runbox.com>
2021-03-12 21:19:31 -0800

    clarified license

--- Esp32-tool/html.h
+++ Esp32-tool/html.h
@@ -1,3 +1,26 @@
+
+/*  Part of esp32-tool.
+ *
+ *  Basic tool to get microprocessor info, configuration, partition tables, 
+ *  etc., and to perform some operations like OTA, clean SPIFFS, etc.
+ *
+ *  Copyright (C) 2021 Mike Marin -- All Rights Reserved
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ *  You can contact me at mmarin <at> acm <dot> org
+ */
+
 #ifndef HTML_H
 #define HTML_H
 
